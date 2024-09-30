6ccfa00d56d20948f6a5898f960401e1a1f887c4
Mayun-binod <shresthabinod334@gmail.com>
2024-09-30 21:58:31 +0545

    tailwind css added configurations

--- chaiaurdjango/chaiaurdjango/settings.py
+++ chaiaurdjango/chaiaurdjango/settings.py
@@ -38,7 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chai',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+TAILWIND_APP_NAME='theme'
+INTERNAL_IPS=['127.0.0.1']
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'chaiaurdjango.urls'
--- chaiaurdjango/chaiaurdjango/urls.py
+++ chaiaurdjango/chaiaurdjango/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('chai/',include('chai.urls')),
+
+
+    path("__reload__/",include("django_browser_reload.urls")),
 ]
