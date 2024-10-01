36caf8f32f7a68278a69444c349882ce4ad0fa62
Mayun-binod <shresthabinod334@gmail.com>
2024-10-01 23:18:09 +0545

    added

--- chaiaurdjango/chai/admin.py
+++ chaiaurdjango/chai/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ChaiVariety
+from .models import ChaiItems
 # Register your models here.
+admin.site.register(ChaiVariety)
+admin.site.register(ChaiItems)
--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -9,7 +9,21 @@ class ChaiVariety(models.Model):
         ('PL','PLAIN'),
         ('EL','ELACHI'),
     ]
-    name=models.CharField(max_length=100),
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='chais/')
+    date_added=models.DateTimeField(default=timezone.now)
+    type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
+
+
+class ChaiItems(models.Model):
+    CHAI_TYPE_CHOICE=[
+        ('ML','MASALA'),
+        ('GR','GINGER'),
+        ('KL','KIWI'),
+        ('PL','PLAIN'),
+        ('EL','ELACHI'),
+    ]
+    name=models.CharField(max_length=100)
     image=models.ImageField(upload_to='chais/')
     date_added=models.DateTimeField(default=timezone.now)
     type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
