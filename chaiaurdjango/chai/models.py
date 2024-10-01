9c3d43e8f64a21cf2f13789ff17f3a9e93b1be48
Mayun-binod <shresthabinod334@gmail.com>
2024-10-01 22:00:17 +0545

    added

--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICE=[
+        ('ML','MASALA'),
+        ('GR','GINGER'),
+        ('KL','KIWI'),
+        ('PL','PLAIN'),
+        ('EL','ELACHI'),
+    ]
+    name=models.CharField(max_length=100),
+    image=models.ImageField(upload_to='chais/')
+    date_added=models.DateTimeField(default=timezone.now)
+    type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
