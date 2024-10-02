5195611a2be49cdde92d80c707989fbfc87c41a5
Mayun-binod <shresthabinod334@gmail.com>
2024-10-02 20:50:16 +0545

    added

--- chaiaurdjango/chai/admin.py
+++ chaiaurdjango/chai/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import ChaiVariety
-from .models import ChaiItems
+from .models import ListChai
+
 # Register your models here.
-admin.site.register(ChaiVariety)
-admin.site.register(ChaiItems)
+
+admin.site.register(ListChai)
--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-class ChaiVariety(models.Model):
+class ListChai(models.Model):
     CHAI_TYPE_CHOICE=[
         ('ML','MASALA'),
         ('GR','GINGER'),
@@ -15,15 +15,6 @@ class ChaiVariety(models.Model):
     type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
 
 
-class ChaiItems(models.Model):
-    CHAI_TYPE_CHOICE=[
-        ('ML','MASALA'),
-        ('GR','GINGER'),
-        ('KL','KIWI'),
-        ('PL','PLAIN'),
-        ('EL','ELACHI'),
-    ]
-    name=models.CharField(max_length=100)
-    image=models.ImageField(upload_to='chais/')
-    date_added=models.DateTimeField(default=timezone.now)
-    type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
+
+    def __str__(self):
+        return(f"{self.name}")
